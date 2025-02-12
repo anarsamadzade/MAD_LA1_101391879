a831b57d96319d67475f26fd037e237fb1ab875c
Anar Samadzade <anar.samadzade@gmail.com>
2025-02-11 21:59:06 -0500

    Initial Commit

---
+++ PrimeChecker/PrimeCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeCheckerApp.swift
+//  PrimeChecker
+//
+//  Created by Tech Atro on 2025-02-11.
+//
+
+import SwiftUI
+
+@main
+struct PrimeCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PrimeCheckerTests/PrimeCheckerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeCheckerTests.swift
+//  PrimeCheckerTests
+//
+//  Created by Tech Atro on 2025-02-11.
+//
+
+import Testing
+@testable import PrimeChecker
+
+struct PrimeCheckerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
